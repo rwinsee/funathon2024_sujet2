a9bcda506e94c1e7ce1e4671754fcff96049730d
romuald <romuald.weidmann@insee.fr>
2024-06-26 11:20:41 +0000

    chgt d'architecture

--- correction/server.R
+++ correction/server.R
@@ -1,8 +1,8 @@
 server <- function(input, output, session) {
-  source("modules_server/module_freq_fr.R", encoding = "UTF-8", local = TRUE)
-  source("modules_server/module_cie_aeriennes.R", encoding = "UTF-8", local = TRUE)
-  source("modules_server/module_liaisons.R", encoding = "UTF-8", local = TRUE)
-  source("modules_server/module_zone.R", encoding = "UTF-8", local = TRUE)
-  source("modules_server/module_fret.R", encoding = "UTF-8", local = TRUE)
-  source("modules_server/module_comparaison_annuelle.R", encoding = "UTF-8", local = TRUE)
+  source("module_freq_fr.R", encoding = "UTF-8", local = TRUE)
+  source("module_cie_aeriennes.R", encoding = "UTF-8", local = TRUE)
+  source("module_liaisons.R", encoding = "UTF-8", local = TRUE)
+  source("module_zone.R", encoding = "UTF-8", local = TRUE)
+  source("module_fret.R", encoding = "UTF-8", local = TRUE)
+  source("module_comparaison_annuelle.R", encoding = "UTF-8", local = TRUE)
 }
