81ff5d5e67db8cfcc2a47b295771a200152efc55
romuald <romuald.weidmann@insee.fr>
2024-07-03 08:12:13 +0000

    passe sur test3 + passage via parquet

--- correction/global.R
+++ correction/global.R
@@ -16,7 +16,8 @@ library(httr)
 library(utils)
 
 # setwd("correction/")#indicates the right WD, otherwise it doesn't run correctly
-source("R/import_data.R")
+source("R/import_data_parquet.R")
+# source("R/import_data_csv.R")
 source("R/create_data_list.R")
 source("R/clean_dataframe.R")
 source("R/divers_functions.R")
@@ -39,9 +40,9 @@ urls <- create_data_list("sources.yml")
 # pax_cie_all <- download_and_read_zip(urls$compagnies$zip) %>% clean_compagnie_data()
 
 # Télécharger et lire les fichiers pour chaque type en Parquet
-pax_apt_all <- import_airport_data_parquet(urls$airports$parquet)
-pax_lsn_all <- import_liaisons_data_parquet(urls$liaisons$parquet)
-pax_cie_all <- import_compagnies_data_parquet(urls$compagnies$parquet)
+pax_apt_all <- import_airport_data_parquet(urls$airports$parquet) %>% clean_airport_data()
+pax_lsn_all <- import_liaisons_data_parquet(urls$liaisons$parquet) %>% clean_liaison_data()
+pax_cie_all <- import_compagnies_data_parquet(urls$compagnies$parquet) %>% clean_compagnie_data()
 
 airports_location <- st_read(urls$geojson$airport)
 
